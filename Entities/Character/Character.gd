9f5bcfb86f35a792d78cab661b9d2b9c9b472895
Fillipe <fillipe.janiques@gmail.com>
2022-01-11 15:41:18 -0300

    added character sprite and animations and particles system

--- Entities/Character/Character.gd
+++ Entities/Character/Character.gd
@@ -4,6 +4,11 @@ const UP = Vector2.UP
 onready var Pos2d = get_tree().current_scene.get_node("GUI/Position")
 var laser = preload("res://Entities/Traps/Laser/Laser.tscn")
 
+onready var animation = $AnimationPlayer
+onready var yellowParticles = $Flip/Particles/YellowParticles
+onready var redParticles = $Flip/Particles/RedParticles
+onready var orangeParticles = $Flip/Particles/OrangeParticles
+
 #MOVEMENT
 var gravity : float = 1000
 var speed : float = 100
@@ -12,6 +17,7 @@ var flySpeed : float = -500
 var jumpForce : float = -500
 var velocity = Vector2.ZERO
 var hp : int = 1
+var goingRight : bool = true
 
 var hVelocity : float = 0
 
@@ -31,8 +37,16 @@ func _physics_process(delta : float) -> void:
 	_jump()
 	_touch_input()
 	_handle_hp()
+	_animation()
 
 func _motion(delta : float):
+	if(touchPos.x > 0):
+		$Flip.scale.x = -1
+		goingRight = true
+	elif(touchPos.x < 0):
+		$Flip.scale.x = 1
+		goingRight = false
+
 	velocity.x = (touchPos.x / 200) * speed
 	if(velocity.y < gravity):
 		velocity.y += gravity * delta 
@@ -47,12 +61,13 @@ func _jump():
 			velocity.y = jumpForce
 
 func _fly():
-	if(Input.is_action_pressed("mouse") and fuel > 0):
-		fuel -= 0.2
-		if(velocity.y > flySpeed):
-			velocity.y += flyForce
-		else:
-			velocity.y = flySpeed
+	if(Input.is_action_pressed("mouse")):
+		if(fuel > 0):
+			fuel -= 0.2
+			if(velocity.y > flySpeed):
+				velocity.y += flyForce
+			else:
+				velocity.y = flySpeed
 
 func _touch_input():
 	if(Input.is_action_pressed("mouse")):
@@ -61,6 +76,30 @@ func _touch_input():
 	else:
 		touching = false
 		touchPos.x = 0
+
+func _animation():
+	if(is_on_floor()):
+		orangeParticles.emitting = false
+		yellowParticles.emitting = false
+		redParticles.emitting = false
+		if(touchPos.x == 0):
+			if(goingRight):
+				animation.play("IdleRight")
+			else:
+				animation.play("IdleLeft")
+		else:
+			if(goingRight):
+				animation.play("RunRight")
+			else:
+				animation.play("RunLeft")
+	else:
+		orangeParticles.emitting = true
+		yellowParticles.emitting = true
+		redParticles.emitting = true
+		if(goingRight):
+			animation.play("FlyRight")
+		else:
+			animation.play("FlyLeft")
 
 func _handle_hp():
 	if(hp <= 0):
